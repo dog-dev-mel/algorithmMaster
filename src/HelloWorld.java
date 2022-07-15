dce8a12d4b0fced8d1bfbee4244de6ef223448b4
ITAIHAO <1388710+dog-dev-mel@users.noreply.github.com>
2022-07-15 21:53:46 +1000

    First Program

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+/*
+ * @Author: ITAIHAO
+ *
+ * @Date: 2022-07-15 21:51
+ *
+ */
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
